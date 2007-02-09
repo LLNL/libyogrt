36cf73482b6fd2042a7bdb473475c219477c6ad9
Christopher J. Morrone <morrone2@llnl.gov>
2007-02-09 02:05:05 +0000

    Add simple test app.

---
+++ test/test1.c
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *  Copyright (C) 2007 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Christopher J. Morrone <morrone2@llnl.gov>.
+ *  UCRL-CODE-XXXXXX.
+ ***************************************************************************
+LICENSE
+ ***************************************************************************/
+
+/*
+ * If libyogrt is installed in a non-standard directory, use the following
+ * to compile:
+ *
+ * gcc foo.c -o foo -I${YOGRT}/include -L${YOGRT}/lib -Wl,--rpath -Wl,${YOGRT}/lib -lyogrt
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+main(int argc, char *argv[])
+{
+	int rem;
+
+	while(1) {
+		rem = yogrt_get_time();
+		if (rem <= 5)
+			break;
+		sleep(1);
+	}
+
+	exit(0);
+}
