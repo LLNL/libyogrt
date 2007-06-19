7c9b50cf664b2547169ee27cb27b10a762ebbff2
Christopher J. Morrone <morrone2@llnl.gov>
2007-06-19 02:06:45 +0000

    Begin adding command to allow 32- AND 64-bit use on AIX w/SLURM.

---
+++ src/slurm/libyogrt_slurm_time.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <slurm/slurm.h>
+
+main()
+{
+	char *jobid_str;
+	uint32_t jobid;
+
+	jobid_str = getenv("SLURM_JOB_ID");
+	if (jobid_str == NULL) {
+		jobid_str = getenv("SLURM_JOBID");
+		if (jobid_str == NULL) {
+			printf("%d\n", MAX_INT);
+			exit(1);
+		}
+	}
+
+	jobid = (uint32_t)atol(jobid_str);
+
+	printf("%ld\n", slurm_get_rem_time(jobid));
+
+	exit(0);
+}
